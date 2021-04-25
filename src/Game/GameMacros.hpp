e395ca2f12d7d3f8b8571fd2b2f2eb5e09b40232
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-04-26 00:40:21 +0200

    Added some game macros

--- src/Game/GameMacros.hpp
+++ src/Game/GameMacros.hpp
@@ -8,6 +8,12 @@
 #ifndef _GAME_MACROS_HPP
 #define _GAME_MACROS_HPP
 
-const float _game_board_size_x = 1600.f;
-const float _game_board_size_y = 900.f;
+const float _game_board_size_x = 16000.f;
+const float _game_board_size_y = 9000.f;
+const float _projection_size_x = 1600.f;
+const float _projection_size_y = 900.f;
+const float _player_size_x = 30.f;
+const float _player_size_y = 15.f;
+const float _player_acceleration = 0.1f;
+const float _player_max_velocity = 3.f;
 #endif
