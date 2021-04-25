1896304ae2e62da80cd7e4e282ec82db34844756
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-04-25 22:23:47 +0200

    Added moves structure

---
+++ src/Game/Moves.hpp
@@ -0,0 +1,18 @@
+/*
+  Author : Dominik Dabkowski
+  Specs  : GNU C++ version 10.2 - "gg+ -std=c++17"
+  Code   : Struct of Directions
+*/
+
+
+#ifndef _DIRECTIONS_HPP
+#define _DIRECTIONS_HPP
+
+
+struct Directions {
+  bool north = false;
+  bool south = false;
+  bool east = false;
+  bool west = false;
+};
+#endif
