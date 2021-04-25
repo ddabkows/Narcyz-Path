381d5997d6ca22d2896ab3f6f6d2723c18ae3fa4
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-04-26 00:14:16 +0200

    Added game macros

---
+++ src/Game/GameMacros.hpp
@@ -0,0 +1,13 @@
+/*
+  Author : Dominik Dabkowski
+  Specs  : GNU C++ version 10.2 - "gg+ -std=c++17"
+  Code   : Game macros
+*/
+
+
+#ifndef _GAME_MACROS_HPP
+#define _GAME_MACROS_HPP
+
+const float _game_board_size_x = 1600.f;
+const float _game_board_size_y = 900.f;
+#endif
