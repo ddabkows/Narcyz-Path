386f38479ee6807d81f17af174398a694bf4b195
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-05-01 16:54:41 +0200

    More relativity

--- src/Game/GameMacros.hpp
+++ src/Game/GameMacros.hpp
@@ -10,8 +10,8 @@
 
 const float _game_board_size_x = 16000.f;
 const float _game_board_size_y = 9000.f;
-const float _projection_size_x = 1600.f;
-const float _projection_size_y = 900.f;
+const float _projection_size_x = _game_board_size_x / 10.f;
+const float _projection_size_y = _game_board_size_y / 10.f;
 const float _player_size_x = 30.f;
 const float _player_size_y = 15.f;
 const float _player_acceleration = 0.1f;
