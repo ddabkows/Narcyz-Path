73b65e38e94c674410e247bc3220f54dfc30c262
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-05-01 17:44:46 +0200

    Completed GameWindow header file

--- src/GUI/GameWindow.hpp
+++ src/GUI/GameWindow.hpp
@@ -10,6 +10,7 @@
 #include "Rectangle.hpp"
 #include "../Game/Dimensions.hpp"
 #include "../Game/GameMacros.hpp"
+#include "../Game/Directions.hpp"
 
 
 #ifndef _GAME_WINDOW_HPP
@@ -31,6 +32,29 @@ class GameWindow {
 
   public:
     // Constructor
-    GameWindow()
+    GameWindow() : _background(_projection_size_x, _projection_size_y, 0.f, 0.f, sf::Color(120, 120, 120, 120), sf::Color(0, 0, 0, 255), 0.f, 0.f),
+                   _player(_player_size_x, _player_size_y, _game_board_size_y - _player_size_y, (_projection_size_x - _player_size_y) / 2.f,
+                   sf::Color(255, 255, 255, 255), sf::Color(0, 0, 0, 255), 0.5f, 0.f) {}
+    
+    // Copy
+    GameWindow(const myClass&) = delete;
+    myClass& operator=(const myClass&) = delete;
+
+    // Move
+    GameWindow(myClass&&) = delete;
+    myClass& operator=(myClass&&) = delete;
+
+    // Methods
+    void drawWindow();
+    void processEvent(sf::Event);
+
+    // Setters
+    void setPlayerPosition(const Dimensions&);
+
+    // Getters
+    const Directions& getPlayerDirections() const;
+
+    // Destructor
+    ~GameWindow() = default;
 };
 #endif
