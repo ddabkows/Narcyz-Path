712951b99be06a96893dfc08f845b6fc64a19508
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-05-12 14:52:11 +0200

    Added a separator of game board

--- src/Game/GameMacros.hpp
+++ src/Game/GameMacros.hpp
@@ -8,10 +8,12 @@
 #ifndef _GAME_MACROS_HPP
 #define _GAME_MACROS_HPP
 
+
 const float _game_board_size_x = 16000.f;
 const float _game_board_size_y = 9000.f;
-const float _projection_size_x = _game_board_size_x / 10.f;
-const float _projection_size_y = _game_board_size_y / 10.f;
+const float _board_divider = 10.f;
+const float _projection_size_x = _game_board_size_x / _board_divider;
+const float _projection_size_y = _game_board_size_y / _board_divider;
 const float _player_size_x = 30.f;
 const float _player_size_y = 15.f;
 const float _player_acceleration = 0.2f;
