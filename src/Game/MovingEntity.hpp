ca78326e574e2988172cc6a199b069e74f4e4e21
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-07-28 19:12:57 +0200

    Deleted player methods from moving entity

--- src/Game/MovingEntity.hpp
+++ src/Game/MovingEntity.hpp
@@ -22,36 +22,24 @@ class MovingEntity : public GameEntity {
     // Trait
     using myClass = MovingEntity;
 
-    Directions _directions{};
+  protected:
     float _max_velocity;
-    float _acceleration;
-    float _velocity = 0.f;
-    std::size_t _quadrant_x;
-    std::size_t _quadrant_y;
+
 
   public:
     // Constructor
     MovingEntity() = default;
 
-    MovingEntity(float size_x, float size_y, float pos_x, float pos_y, float acceleration, float max_velocity) : 
+    MovingEntity(float size_x, float size_y, float pos_x, float pos_y, float max_velocity) : 
                                                                       GameEntity(size_x, size_y, pos_x, pos_y),
-                                                                      _max_velocity(max_velocity),
-                                                                      _acceleration(acceleration),
-                                                                      _quadrant_x(static_cast<std::size_t>(_position.x / _projection_size_x)),
-                                                                      _quadrant_y(static_cast<std::size_t>(_position.y / _projection_size_y)) {}
+                                                                      _max_velocity(max_velocity)
+                                                                       {}
 
     // Copy
     MovingEntity(const myClass& moving_entity) : GameEntity(moving_entity),
-                                                 _directions(moving_entity._directions),
-                                                 _max_velocity(moving_entity._max_velocity),
-                                                 _acceleration(moving_entity._acceleration),
-                                                 _velocity(moving_entity._velocity),
-                                                 _quadrant_x(moving_entity._quadrant_x),
-                                                 _quadrant_y(moving_entity._quadrant_y) {
+                                                 _max_velocity(moving_entity._max_velocity) {
       _position = moving_entity._position;
       _size = moving_entity._size;
-      _directions = moving_entity._directions;
-      _velocity = moving_entity._velocity;
     }
     myClass& operator=(const myClass&) = delete;
 
@@ -60,16 +48,11 @@ class MovingEntity : public GameEntity {
     myClass& operator=(myClass&&) = delete;
 
     // Methods
-    void horizontalMove(int, const std::vector<std::shared_ptr<GameEntity>>);
-    void verticalMove(int, const std::vector<std::shared_ptr<GameEntity>>);
-    void oblicMove(int, int, const std::vector<std::shared_ptr<GameEntity>>);
-    void move(Directions, const std::vector<std::vector<std::vector<std::shared_ptr<GameEntity>>>>);
+    
 
     // Setters
 
     // Getters
-    const std::size_t& getQuadrantX() const;
-    const std::size_t& getQuadrantY() const;
     
     // Destructor
     ~MovingEntity() = default;
