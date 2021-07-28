0961798c9cf0fa495af415cbce1cb9128481dffa
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-07-28 18:03:58 +0200

    Created GUI macros

---
+++ src/GUI/GUIMacros.hpp
@@ -0,0 +1,15 @@
+/*
+  Author : Dominik Dabkowski
+  Specs  : GNU C++ version 10.2 - "gg+ -std=c++17"
+  Code   : Game macros
+*/
+
+
+#include <string>
+
+
+#ifndef _GUI_MACROS_HPP
+#define _GUI_MACROS_HPP
+
+const std::string _font_1_path = "../fonts/MiddAges.ttf";
+#endif
