62f7bd09fc16451c938a87888a66967a59f930b2
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-07-28 21:17:07 +0200

    Created a mob class

---
+++ src/Game/Mob.cpp
@@ -0,0 +1,14 @@
+/*
+  Author : Dominik Dabkowski
+  Specs  : GNU C++ version 10.2 - "gg+ -std=c++17"
+  Code   : Mob methods
+*/
+
+
+#include "Mob.hpp"
+
+
+// Methods
+void Mob::move(std::shared_ptr<GameEntity> player, const std::vector<std::shared_ptr<GameEntity>> walls) {
+
+}
---
+++ src/Game/Mob.hpp
@@ -0,0 +1,49 @@
+/*
+  Author : Dominik Dabkowski
+  Specs  : GNU C++ version 10.2 - "gg+ -std=c++17"
+  Code   : Class of Mob
+*/
+
+
+#include <vector>
+#include <memory>
+
+#include "MovingEntity.hpp"
+#include "GameMacros.hpp"
+
+
+#ifndef _MOB_HPP
+#define _MOB_HPP
+
+
+class Mob : public MovingEntity {
+  private:
+    // Trait
+    using myClass = Mob;
+
+  public:
+    // Constructor
+    Mob() = default;
+
+    Mob(float size_x, float size_y, float pos_x, float pos_y, float max_velocity, std::size_t quadrant_x, std::size_t quadrant_y) : 
+                                                                                  MovingEntity(size_x, size_y, pos_x, pos_y, max_velocity, quadrant_x, quadrant_y) {}
+
+    // Copy
+    Mob(const myClass& mob) : MovingEntity(mob) {
+      _position = mob._position;
+      _size = mob._size;
+    }
+
+    myClass& operator=(const myClass&) = delete;
+
+    // Move
+    Mob(myClass&& mob) : MovingEntity(mob) {
+      _position = mob._position;
+      _size = mob._size;
+    }
+    myClass& operator=(myClass&&) = delete;
+
+    // Methods
+    void move(std::shared_ptr<GameEntity>, const std::vector<std::shared_ptr<GameEntity>>);
+};
+#endif
