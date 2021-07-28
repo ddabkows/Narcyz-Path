8a95e9fb5071f023070a98ceae6905b8b6ceebdc
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-07-28 22:03:52 +0200

    Added spawner class

---
+++ src/Game/Spawner.cpp
@@ -0,0 +1,23 @@
+/*
+  Author : Dominik Dabkowski
+  Specs  : GNU C++ version 10.2 - "gg+ -std=c++17"
+  Code   : Mob methods
+*/
+
+
+#include "Spawner.hpp"
+
+
+// Methods
+void Spawner::spawn_mobs(float time) {
+  if (_mobs.size() < _max_mobs && _spawn_speed < (time - _last_spawn)) {
+    float pos_x = (_projection_size_x * _quadrant_x) + _board_divider + (rand()%static_cast<int>(_projection_size_x - _board_divider - _mob_size.x));
+    float pos_y = (_projection_size_y * _quadrant_y) + _board_divider + (rand()%static_cast<int>(_projection_size_y - _board_divider - _mob_size.y));
+    _mobs.emplace_back(std::make_shared<Mob>(_mob_size.x, _mob_size.y, pos_x, pos_y, _mob_velocity, _quadrant_x, _quadrant_y));
+    _last_spawn = time;
+  } 
+}
+
+void Spawner::despawn_mobs() {
+  _mobs.clear();
+}
---
+++ src/Game/Spawner.hpp
@@ -0,0 +1,61 @@
+/*
+  Author : Dominik Dabkowski
+  Specs  : GNU C++ version 10.2 - "gg+ -std=c++17"
+  Code   : Class of Spawner
+*/
+
+
+#include <vector>
+#include <memory>
+#include <random>
+
+#include "Mob.hpp"
+#include "GameEntity.hpp"
+#include "GameMacros.hpp"
+
+
+#ifndef _SPAWNER_HPP
+#define _SPAWNER_HPP
+
+
+class Spawner {
+  private:
+    // Trait
+    using myClass = Spawner;
+
+    std::size_t _quadrant_x;
+    std::size_t _quadrant_y;
+
+    Dimensions _mob_size;
+    float _mob_velocity;
+    std::size_t _max_mobs;
+    std::vector<std::shared_ptr<Mob>> _mobs;
+
+    float _spawn_speed;
+    float _last_spawn = 0.f;
+
+  public:
+    // Constructor
+    Spawner() = default;
+
+    Spawner(std::size_t quadrant_x, std::size_t quadrant_y, float size_x, float size_y, float mob_velocity, std::size_t max_mobs, float spawn_speed) : 
+                                                                                                                                  _quadrant_x(quadrant_x),
+                                                                                                                                  _quadrant_y(quadrant_y),
+                                                                                                                                  _mob_size(size_x, size_y),
+                                                                                                                                  _mob_velocity(mob_velocity),
+                                                                                                                                  _max_mobs(max_mobs),
+                                                                                                                                  _spawn_speed(spawn_speed) {}
+
+    // Copy
+    Spawner(const myClass&) = delete;
+    myClass& operator=(const myClass&) = delete;
+
+    // Move
+    Spawner(myClass&&) = delete;
+    myClass& operator=(myClass&&) = delete;
+
+    // Methods
+    void spawn_mobs(float);
+    void despawn_mobs();
+};
+#endif
