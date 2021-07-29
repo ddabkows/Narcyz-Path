7962bfe6c0568300d77cfd80bda4e9c41091daaa
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-07-30 01:00:58 +0200

    Added death situation

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -19,38 +19,42 @@ void Game::generateGame() {
   
 }
 
-void Game::updatePlayer(Directions updating_directions) {
-  std::vector<std::shared_ptr<GameEntity>> mobs{};
-  for (std::size_t spawner_index = 0; spawner_index < _mob_spawners.size(); ++spawner_index) {
-    if (_mob_spawners[spawner_index]->getQuadrantX() == _player->getQuadrantX() && _mob_spawners[spawner_index]->getQuadrantY() == _player->getQuadrantY()) {
-      for (std::size_t mob_index = 0; mob_index < _mob_spawners[spawner_index]->getMobs().size(); ++mob_index) {
-        mobs.emplace_back(_mob_spawners[spawner_index]->getMobs()[mob_index]);
-      }
-    } 
-  }
+void Game::updateGame(Directions updating_directions) {
+  endGame();
+  if (!_is_over) {
+    std::vector<std::shared_ptr<GameEntity>> mobs{};
+    for (std::size_t spawner_index = 0; spawner_index < _mob_spawners.size(); ++spawner_index) {
+      if (_mob_spawners[spawner_index]->getQuadrantX() == _player->getQuadrantX() && _mob_spawners[spawner_index]->getQuadrantY() == _player->getQuadrantY()) {
+        for (std::size_t mob_index = 0; mob_index < _mob_spawners[spawner_index]->getMobs().size(); ++mob_index) {
+          mobs.emplace_back(_mob_spawners[spawner_index]->getMobs()[mob_index]);
+        }
+      } 
+    }
 
-  if (_game_timer.getElapsedTime().asSeconds() - _player_walk_timer > 0.1) {
-    _player->move(updating_directions, mobs, _walls);
-    checkSpawners();
-    _player_walk_timer = _game_timer.getElapsedTime().asSeconds();
+    float time = _game_timer.getElapsedTime().asSeconds();
+    if (time - _player_walk_timer > _game_tick) {
+      _player->move(updating_directions, mobs, _walls);
+      checkSpawners();
+      _player_walk_timer = _game_timer.getElapsedTime().asSeconds();
+    }
   }
 }
 
 void Game::checkSpawners() {
+  float time = _game_timer.getElapsedTime().asSeconds();
   for (std::size_t spawner_index = 0; spawner_index < _mob_spawners.size(); ++spawner_index) {
     std::size_t dist_x = checkDist(_player->getQuadrantX(), _mob_spawners[spawner_index]->getQuadrantX());
     std::size_t dist_y = checkDist(_player->getQuadrantY(), _mob_spawners[spawner_index]->getQuadrantY());
     if (dist_x == 0 && dist_y == 0) {
       std::vector<std::shared_ptr<Mob>> mobs_to_move = _mob_spawners[spawner_index]->getMobs();
       for (std::size_t mob_index = 0; mob_index < mobs_to_move.size(); ++mob_index) {
-        GameEntity player = *_player;
         std::vector<std::shared_ptr<Mob>> other_mobs = mobs_to_move;
         other_mobs.erase(other_mobs.begin() + static_cast<int>(mob_index));
-        mobs_to_move[mob_index]->move(std::make_shared<GameEntity>(player), other_mobs, _walls[_player->getQuadrantX()][_player->getQuadrantY()]);
+        mobs_to_move[mob_index]->move(_player, other_mobs, _walls[_player->getQuadrantX()][_player->getQuadrantY()], time);
       }
     }
     if (dist_x <= _spawn_dist && dist_y <= _spawn_dist) {
-      _mob_spawners[spawner_index]->spawn_mobs(_game_timer.getElapsedTime().asSeconds(), _walls[_mob_spawners[spawner_index]->getQuadrantX()][_mob_spawners[spawner_index]->getQuadrantY()]);
+      _mob_spawners[spawner_index]->spawn_mobs(time, _walls[_mob_spawners[spawner_index]->getQuadrantX()][_mob_spawners[spawner_index]->getQuadrantY()]);
     }
     else if (dist_x > _despawn_dist || dist_y > _despawn_dist) {
       _mob_spawners[spawner_index]->despawn_mobs();
@@ -62,6 +66,10 @@ std::size_t Game::checkDist(std::size_t a_quadrant, std::size_t b_quadrant) {
   return static_cast<std::size_t>(abs(static_cast<int>(a_quadrant - b_quadrant)));
 }
 
+void Game::endGame() {
+  if (_player->getHealth() <= 0) {_is_over = true;}
+}
+
 
 // Getters
 const Dimensions& Game::getPlayerDimensions() const {return _player->getPosition();}
@@ -69,3 +77,7 @@ const std::vector<std::shared_ptr<GameEntity>>& Game::getWalls(std::size_t quadr
 const std::size_t& Game::getPlayerQuadrantX() const {return _player->getQuadrantX();}
 const std::size_t& Game::getPlayerQuadrantY() const {return _player->getQuadrantY();}
 const std::vector<std::shared_ptr<Spawner>>& Game::getSpawners() const {return _mob_spawners;}
+const Dimensions& Game::getPlayerSize() const {return _player->getSize();}
+float Game::getPlayerHealth() const {return _player->getHealthProportion();}
+const bool& Game::isOver() const {return _is_over;}
+float Game::getGameTimer() const {return _game_timer.getElapsedTime().asSeconds();}
--- src/Game/Game.hpp
+++ src/Game/Game.hpp
@@ -18,6 +18,7 @@
 #include "Spawner.hpp"
 #include "GameMacros.hpp"
 #include "GameWalls.hpp"
+#include "AttackToDisplay.hpp"
 
 
 #ifndef _GAME_HPP
@@ -36,7 +37,12 @@ class Game {
 
     float _player_walk_timer = 0.f;
     sf::Clock _game_timer{};
-    std::vector<std::shared_ptr<Spawner>> _mob_spawners = {std::make_shared<Spawner>(std::size_t(0), std::size_t(8), 15.f, 30.f, 8.f, std::size_t(15), 3.f)};
+    bool _is_over = false;
+    std::vector<std::shared_ptr<Spawner>> _mob_spawners = {std::make_shared<Spawner>(_classic_mob_spawn_1_x, _classic_mob_spawn_1_y, 
+                                                           _player_size_x, _player_size_y, _classic_mob_velocity, _classic_mob_max_count, 
+                                                           _classic_mob_spawn_speed, _classic_mob_max_hp, _classic_mob_hit_charge, _classic_mob_hit_cooldown,
+                                                           _classic_mob_attack_strength,
+                                                           AttackToDisplay(_classic_mob_attack_size_x, _classic_mob_attack_size_y, _classic_mob_hit_display_time))};
 
   public:
   // Constructor
@@ -55,9 +61,10 @@ class Game {
 
   // Methods
   void generateGame();
-  void updatePlayer(Directions);
+  void updateGame(Directions);
   void checkSpawners();
   std::size_t checkDist(std::size_t, std::size_t);
+  void endGame();
 
   // Setters
 
@@ -66,7 +73,11 @@ class Game {
   const std::vector<std::shared_ptr<GameEntity>>& getWalls(std::size_t, std::size_t) const;
   const std::size_t& getPlayerQuadrantX() const;
   const std::size_t& getPlayerQuadrantY() const;
+  const Dimensions& getPlayerSize() const;
   const std::vector<std::shared_ptr<Spawner>>& getSpawners() const;
+  float getPlayerHealth() const;
+  const bool& isOver() const;
+  float getGameTimer() const;
 
   // Destructor
   ~Game() = default;
