36e609118b7b8fa83c1f06a4d1a37ac4eea2355e
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-07-30 20:06:37 +0200

    Added the text message string

--- src/GUI/GUIMacros.hpp
+++ src/GUI/GUIMacros.hpp
@@ -14,4 +14,5 @@
 const std::string _font_1_path = "../fonts/MiddAges.ttf";
 const float _hp_display_prop   = 3.f;
 const float _hp_display_height = 3.f;
+const std::string _spawn_text = "Hello! If you are reading this, you are\nnot the kind of people who read the\nREADME files, huh? Or are you the kind\nof people who do not like anyone to tell\nthem what to do? Well, whatever it is\nit's not important right now\n(but read the README bro, it's funny)\n\nControls:\nMove-WSAD/ZQSD\nAttacks-arrow keys";
 #endif
