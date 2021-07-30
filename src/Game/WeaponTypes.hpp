fd12ebe6516ae946ebf09a7a0ad5a3cad52566e0
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-07-30 02:25:13 +0200

    Added weapon types struct

---
+++ src/Game/WeaponTypes.hpp
@@ -0,0 +1,15 @@
+/*
+  Author : Dominik Dabkowski
+  Specs  : GNU C++ version 10.2 - "gg+ -std=c++17"
+  Code   : Struct of Weapon types
+*/
+
+
+#ifndef _WEAPON_TYPES_HPP
+#define _WEAPON_TYPES_HPP
+
+
+enum WeaponTypes {
+  classic
+};
+#endif
