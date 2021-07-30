6248f99415cffaa2b903e0a9f0265e89593a4c4c
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-07-30 21:22:15 +0200

    Changed the spawn message

--- src/GUI/GUIMacros.hpp
+++ src/GUI/GUIMacros.hpp
@@ -14,6 +14,6 @@
 const std::string _font_1_path = "../fonts/MiddAges.ttf";
 const float _hp_display_prop   = 3.f;
 const float _hp_display_height = 3.f;
-const std::string _spawn_text = "Hello! If you are reading this, you are\nnot the kind of people who read the\nREADME files, huh? Or are you the kind\nof people who do not like anyone to tell\nthem what to do? Well, whatever it is\nit's not important right now\n(but read the README bro, it's funny)\n\nControls:\nMove-WSAD/ZQSD\nAttacks-arrow keys";
+const std::string _spawn_text = "Hello! If you are reading this, you are\nnot the kind of people who read the\nREADME files, huh? Or are you the kind\nof people who do not like anyone to tell\nthem what to do? Well, whatever it is\nit's not important right now\n(but read the README bro, it's funny)\n\nControls:\nMove-WSAD/ZQSD\nAttacks-arrow keys\nExit-escape";
 const std::string _swap_keys_text = "WSAD/ZQSD";
 #endif
