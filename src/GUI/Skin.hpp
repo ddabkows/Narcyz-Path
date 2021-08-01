e6d4012b2033d51565410825d160a26d8dc8d14d
Dominik Dabkowski <dominik.dabkowski@ulb.be>
2021-08-01 21:00:49 +0200

    Added skin enum

---
+++ src/GUI/Skin.hpp
@@ -0,0 +1,16 @@
+/*
+  Author : Dominik Dabkowski
+  Specs  : GNU C++ version 10.2 - "gg+ -std=c++17"
+  Code   : Struct of Skin
+*/
+
+
+#ifndef _SKIN_HPP
+#define _SKIN_HPP
+
+
+enum Skin {
+  no_skin = 0,
+  classic_mob_attack = 1
+};
+#endif
