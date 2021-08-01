1ab79c7fe119466bf135f87adb230048994e48c0
Dominik Dabkowski <dominik.dabkowski@ulb.be>
2021-08-01 14:52:44 +0200

    Took pointer to the game window for later changes

--- src/Client/main.cpp
+++ src/Client/main.cpp
@@ -12,19 +12,18 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Window.hpp>
 
-
 int main() {
-  GameWindow game_win{};
+  std::shared_ptr<GameWindow> game_win = std::make_shared<GameWindow>();
   bool keep_program_open = true;
   
   while (keep_program_open) {
     sf::Event event;
 
-    while (game_win.pollEvent(event)) {game_win.processEvent(event);}
-    game_win.concludeEvent();
-    game_win.updateWindow();
+    while (game_win->pollEvent(event)) {game_win->processEvent(event);}
+    game_win->concludeEvent();
+    game_win->updateWindow();
 
-    if (!game_win.getOpen()) {
+    if (!game_win->getOpen()) {
       keep_program_open = false;
     }
   }
