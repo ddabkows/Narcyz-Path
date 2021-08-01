7441b9ca681e0a6064a27481e4325a4a0df3b578
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-08-01 19:41:14 +0200

    ClassicWeapon class now adapts display position to GUI

--- src/Game/ClassicWeapon.cpp
+++ src/Game/ClassicWeapon.cpp
@@ -16,14 +16,29 @@ void ClassicWeapon::attack(std::vector<std::shared_ptr<MovingEntity>> mobs, floa
     if (_attack_display.attack_direction == west || _attack_display.attack_direction == east) {
       if (_attack_display.size.x < _attack_display.size.y) {_attack_display.size = Dimensions(_attack_display.size.y, _attack_display.size.x);}
       _attack_display.pos.y = _attack_display.pos.y - ((_attack_display.size.y - _player_size_y) / 2.f);
-      if (_attack_display.attack_direction == west) {_attack_display.pos.x = _attack_display.pos.x - _attack_display.size.x;}
-      else {_attack_display.pos.x = _attack_display.pos.x + _player_size_x;}
+      if (_attack_display.attack_direction == west) {
+        _attack_display.pos.x = _attack_display.pos.x - _attack_display.size.x;
+        _attack_display.display_pos.x = _attack_display.pos.x + _attack_display.starting_size.x;
+        _attack_display.display_pos.y = _attack_display.pos.y + _attack_display.starting_size.y;  
+      }
+      else {
+        _attack_display.pos.x = _attack_display.pos.x + _player_size_x;
+        _attack_display.display_pos = _attack_display.pos;
+      }
     }
     else {
       if (_attack_display.size.x > _attack_display.size.y) {_attack_display.size = Dimensions(_attack_display.size.y, _attack_display.size.x);}
       _attack_display.pos.x = _attack_display.pos.x - ((_attack_display.size.x - _player_size_x) / 2.f);
-      if (_attack_display.attack_direction == north) {_attack_display.pos.y = _attack_display.pos.y - _attack_display.size.y;}
-      else {_attack_display.pos.y = _attack_display.pos.y + _player_size_y;}
+      if (_attack_display.attack_direction == north) {
+        _attack_display.pos.y = _attack_display.pos.y - _attack_display.size.y;
+        _attack_display.display_pos.x = _attack_display.pos.x;
+        _attack_display.display_pos.y = _attack_display.pos.y + _attack_display.starting_size.x;
+      }
+      else {
+        _attack_display.pos.y = _attack_display.pos.y + _player_size_y;
+        _attack_display.display_pos.x = _attack_display.pos.x + _attack_display.starting_size.y;
+        _attack_display.display_pos.y = _attack_display.pos.y;
+      }
     }
     for (std::size_t mob_index = 0; mob_index < mobs.size(); ++mob_index) {
       if (_attack_display.pos.x <= mobs[mob_index]->getPosition().x + mobs[mob_index]->getSize().x && mobs[mob_index]->getPosition().x <= _attack_display.pos.x + _attack_display.size.x &&
