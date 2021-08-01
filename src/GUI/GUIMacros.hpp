309811acd0285c9b6a2d597d121d884b13bcc6d0
Dominik Dabkowski <dominik.dabkowski@ulb.be>
2021-08-01 21:01:13 +0200

    Gui macros for classic mob attacks

--- src/GUI/GUIMacros.hpp
+++ src/GUI/GUIMacros.hpp
@@ -12,12 +12,14 @@
 #define _GUI_MACROS_HPP
 
 const std::string _font_1_path           = "../fonts/MiddAges.ttf";
-const std::string _magic_ball_path       = "../img/sprites/player/MagicBallSpriteAnimation.png";
+const std::string _magic_ball_path       = "../img/sprites/player/attack_1.png";
+const std::string _mob_attack_1_path     = "../img/sprites/mob_attacks/1.png";
 const float _hp_display_prop             = 3.f;
 const float _hp_display_height           = 3.f;
 const std::string _spawn_text            = "Hello! If you are reading this, you are\nnot the kind of people who read the\nREADME files, huh? Or are you the kind\nof people who do not like anyone to tell\nthem what to do? Well, whatever it is\nit's not important right now\n(but read the README bro, it's funny)\n\nControls:\nMove-WSAD/ZQSD\nAttacks-arrow keys\nExit-escape";
 const std::string _swap_keys_text        = "WSAD/ZQSD";
 const unsigned int _magic_ball_texture_x = 9;
 const unsigned int _magic_ball_texture_y = 1;
-const float _magic_ball_texture_scale = 0.1f;
+const unsigned int _mob_attack_1_x       = 9;
+const unsigned int _mob_attack_1_y       = 1;
 #endif
