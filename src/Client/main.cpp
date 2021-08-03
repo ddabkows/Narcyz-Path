c94ee18dfb83a0b3c79e9aa24c7216856ff69176
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-08-03 02:06:26 +0200

    Adapted main file to GUI

--- src/Client/main.cpp
+++ src/Client/main.cpp
@@ -5,29 +5,14 @@
 */
 
 
-#include "../GUI/GameWindow.hpp"
-#include "../GUI/Master.hpp"
+#include "../GUI/GUI.hpp"
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <SFML/Window.hpp>
 
 int main() {
-  std::shared_ptr<Game> game = std::make_shared<Game>();
-  std::shared_ptr<sf::RenderWindow> window = std::make_shared<sf::RenderWindow>();
-  std::shared_ptr<GameWindow> game_win = std::make_shared<GameWindow>(window, game);
-  bool keep_program_open = true;
-  
-  while (keep_program_open) {
-    sf::Event event;
-
-    while (game_win->pollEvent(event)) {game_win->processEvent(event);}
-    game_win->concludeEvent();
-    game_win->updateWindow();
-
-    if (!game_win->getOpen()) {
-      keep_program_open = false;
-    }
-  }
+  GUI gui{};
+  gui.displayMenus();
   return 0;
 }
